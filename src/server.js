a47863667456c583b73bd2b014fb8e21e9fe572b
Ayeshaali124 <ayeshansari124@gmail.com>
2025-12-04 14:22:50 +0530

    deployment tweaks

--- src/server.js
+++ src/server.js
@@ -4,4 +4,13 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => app.listen(PORT));
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Database connection failed", err);
+    process.exit(1);
+  });
